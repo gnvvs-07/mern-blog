dc62d7b06038226490ed84974a208f2eca92e72d
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-11 11:51:19 +0530

    created all required pages and declared all their paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-export default function App(){
-  return(
-    <h1 className="text-3xl text-red-500">
-      App
-    </h1>
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
+export default function App() {
+  return (
+    // all routing path declarations goes here
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
