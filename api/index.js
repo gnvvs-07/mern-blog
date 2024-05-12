eaa56b436103863ee9d53c4c8887e68f230c642b
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-12 09:29:23 +0530

    backend connected to mongodb - success

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => { console.log("MongoDB is connected"); })
+  .catch((error) => { console.error("MongoDB connection error:", error); });
 
 const app = express();
 
-app.listen(3000,()=>{console.log("server is running in port 3000")})
+app.listen(3000, () => { console.log("Server is running on port 3000"); });
