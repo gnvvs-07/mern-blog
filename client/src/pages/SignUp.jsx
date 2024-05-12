e32cb2275e5dddabf1661300f4d2d75ba8259b2c
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-12 23:44:58 +0530

    sign up functionality-success

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,46 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  }; // form data
+
+  // submtting the form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill all the fields");
+    }
+    // submit to backend
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      // console.log(error);
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex gap-4 p-4 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
@@ -20,7 +59,7 @@ export default function SignUp() {
         </div>
         {/* right side container  */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="User name" />
               <TextInput
@@ -28,29 +67,44 @@ export default function SignUp() {
                 placeholder="Enter your user name"
                 id="username"
                 className=""
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="email@company.com"
                 id="email"
                 className=""
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Password" />
               <TextInput
-                type="text"
+                type="password"
                 placeholder="Enter your password"
                 id="password"
                 className=""
+                onChange={handleChange}
               />
             </div>
             {/* submit button */}
-            <Button gradientDuoTone="purpleToBlue" outline type="submit">
-              SignUp
+            <Button
+              gradientDuoTone="purpleToBlue"
+              outline
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading</span>
+                </>
+              ) : (
+                "SignUp"
+              )}
             </Button>
           </form>
           <div className="flex flex-row gap-3 text-sm">
@@ -59,6 +113,11 @@ export default function SignUp() {
               Login
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-2" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Add your proxy configuration here
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+});
