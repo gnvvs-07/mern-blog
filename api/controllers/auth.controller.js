4345d085aa27d85d14d4d78ce871a5ab22691a20
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-14 10:18:06 +0530

    sign in routing and verification of user , cookie setting - completed.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import dotenv from "dotenv";
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body; //accessing user details
   if (
     !username ||
@@ -12,7 +15,7 @@ export const signup = async (req, res,next) => {
     password === ""
   ) {
     // return res.status(400).json({ message: "all fields are required" });
-    next(errorHandler(400,"all fields must be filled / required"))
+    next(errorHandler(400, "all fields must be filled / required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
@@ -24,6 +27,37 @@ export const signup = async (req, res,next) => {
     await newUser.save();
     res.json("signup-success");
   } catch (error) {
-    next(error);    //middle ware 
+    next(error); //middle ware
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body; // Accessing user details
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields must be filled / required"));
+  }
+  try {
+    // Finding a user
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found")); // No valid user exists
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Password and email combination"));
+    }
+    // Separating sensitive fields from user data
+    const { password: pass, ...others } = validUser._doc;
+    dotenv.config();
+    // Generating a JWT token with an expiration time (e.g., 1 hour)
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" } // Token expires in 1 hour
+    );
+    // Setting the token as an HttpOnly cookie
+    res.cookie("userToken", token, { httpOnly: true }).json(others);
+  } catch (error) {
+    next(error); // Middleware to handle errors
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup,signin } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup); //sign up router
-
+router.post("/signin", signin); //sign up router
 export default router;
