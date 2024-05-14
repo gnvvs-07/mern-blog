73bc61c670be8f52dcc12a5bfa98c1cfcf82157f
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-14 20:50:22 +0530

    react persist functionality- success

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import useReducer from './user/userSlice.js';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js"; // Renamed 'useReducer' to 'userReducer'
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { persistStore } from "redux-persist";
+
+// Root reducer
+const rootReducer = combineReducers({
+  user: userReducer, // Updated to use 'userReducer'
+});
+
+// Configuration for persisting the state
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user:useReducer,
-  },
-})
+  reducer: persistedReducer, // Corrected to use 'persistedReducer'
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
