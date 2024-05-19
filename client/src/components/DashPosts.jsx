7700e6fedd6193141000c4b98105f0fef25d9bb8
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-19 11:08:47 +0530

    posts showing in the posts page

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,5 +1,80 @@
+import { Table } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 export default function DashPosts() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
   return (
-    <div>DashPosts</div>
-  )
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-200 scrollbar-thumb-slate-400 dark:scrollbar-thumb-slate-800 dark:scrollbar-track-slate-600">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post Image</Table.HeadCell>
+              <Table.HeadCell>Post Title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>Edit</Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {userPosts.map((post) => (
+                <Table.Row key={post._id}>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt="Post"
+                        className="h-10 w-20 object-cover"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`post/${post.slug}`}>
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell className="text-blue-500">{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="text-red-500 hover:underline">Delete</span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`update-post/${post._id}`} className="text-green-500 hover:underline" >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </>
+      ) : (
+        <p>No posts yet</p>
+      )}
+    </div>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-import flowbite from "flowbite-react/tailwind"
+import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(),],
-}
+  plugins: [
+    flowbite.plugin(),
+    tailwindScrollbar, // Directly import and include the plugin
+  ],
+};
