8e47c59627fe8a3d05e88595d69f06da39c86484
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-22 23:57:53 +0530

    pre-installation complete

--- api/index.js
+++ api/index.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import postRoutes from"./routes/post.route.js";
+import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -25,6 +26,8 @@ mongoose
     console.error("MongoDB connection error:", error);
   });
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
@@ -34,8 +37,14 @@ app.listen(PORT, () => {
 // Testing a routing
 app.use("/api/user", userRoutes); //localhost:3000/api/user/test
 app.use("/api/auth", authRoutes);
-app.use("/api/post",postRoutes);
-app.use("/api/comment",commentRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
