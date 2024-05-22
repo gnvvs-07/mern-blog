d635385910643c38c2b6b57a10b9f32e8ce2a064
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-22 11:14:40 +0530

    added comments section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import {
   HiArrowCircleRight,
   HiOutlineDocumentText,
   HiOutlineUserGroup,
+  HiAnnotation,
 } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -70,15 +71,26 @@ export default function DashSidebar() {
           </Link>
         )}
         {currentUser.isAdmin && (
-          <Link to="/dashboard?tab=users">
-            <Sidebar.Item
-              active={tab === "users"}
-              icon={HiOutlineUserGroup}
-              as="div"
-            >
-              Users
-            </Sidebar.Item>
-          </Link>
+          <>
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item
+                active={tab === "users"}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
+            <Link to="/dashboard?tab=comments">
+              <Sidebar.Item
+                active={tab === "comments"}
+                icon={HiAnnotation}
+                as="div"
+              >
+                Comments
+              </Sidebar.Item>
+            </Link>
+          </>
         )}
         <Sidebar.Item
           icon={HiArrowCircleRight}
